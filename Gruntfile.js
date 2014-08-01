46babc4f1ab25b4306f50a1b087d60d721063590
Michael Grabski <mike@BartMancuso2.local>
2014-08-01 02:06:49 -0400

    Create gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+    // Test settings
+    karma: {
+      unit: {
+        configFile: 'test/karma.conf.js',
+        singleRun: true
+      }
+    }
+  });
+
+  grunt.registerTask('test', [
+    'karma'
+  ]);
+
+  grunt.registerTask('default', [
+    'test'
+  ]);
+};
